96fe2f9347e5814e007ce6e0148a7de6ff22fb0c
trah0018 <trah0018@algonquinlive.com>
2025-06-18 17:42:05 -0400

    Completes the assignment

--- t-shirts.js
+++ t-shirts.js
@@ -1,4 +1,5 @@
-const tshirts = [
+
+ const tshirts = [
   {
     title: 'Blue T-Shirt',
     image: 'blue-t-shirt.jpg',
@@ -62,4 +63,76 @@ const tshirts = [
     stock: 2,
     quantity: 1
   }
-]
+] 
+
+
+
+
+  const App = () => {
+    
+const [tshirtsState, setTshirtsState] = React.useState(tshirts);
+const handleQuantityChange = (index, value) => {
+  setTshirtsState((prev) => {
+    const newTshirts = [...prev];
+    newTshirts[index].quantity = Number(value);
+    return newTshirts;
+  });
+};
+
+
+const handleBuy = (index) => {
+  setTshirtsState((prev) => {
+    const newTshirts = [...prev];
+    const qty = newTshirts[index].quantity || 1;
+
+    if (qty <= newTshirts[index].stock) {
+      newTshirts[index].stock -= qty;
+     
+      newTshirts[index].quantity = newTshirts[index].stock > 0 ? 1 : 0;
+    }
+    return newTshirts;
+  });
+};
+
+    
+
+    return (
+        <div>
+            <h1>T-shirt Store</h1>
+            <ul>
+                {tshirtsState.map((tshirt, index) => (
+                    <li key={index}>
+                        <h3>{tshirt.title}</h3>
+                        <img src={`./images/${tshirt.image}`} alt={tshirt.title} width='150'/>
+                        <p>Price: ${tshirt.price.toFixed(2)}</p>
+                        {tshirt.title} - ${tshirt.price.toFixed(2)}
+                        <p>{tshirt.stock > 0
+                          ? `Stock: ${tshirt.stock}` : 'Out of Stock'}</p>
+                    
+                     {tshirt.stock > 0 && (
+                      <>
+                      <select 
+                      value={tshirt.quantity || 1}
+                      onChange={(e) => { handleQuantityChange(index, e.target.value) }}
+                      >
+                      {[...Array(tshirt.stock).keys()].map((num) => (
+                        <option key={num + 1} value={num + 1}>
+                          {num + 1}
+                        </option>
+                      ))}
+                      
+                      </select>
+                      <button onClick={() => handleBuy(index)}>Buy</button>
+                        
+                      </>
+                    )}
+                    </li>
+            ))}
+            </ul>
+                
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
